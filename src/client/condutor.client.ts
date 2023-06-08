41df03e1bad26a06a29aef2eacdaa27eaeff6ee3
Kauê Sanches <kauesanches123@gmail.com>
2023-06-07 21:12:20 -0300

    Inicio das clients

---
+++ src/client/condutor.client.ts
@@ -0,0 +1,40 @@
+import { Condutor } from "@/model/condutor";
+import axios ,{ AxiosInstance } from "axios";
+
+export class CondutorClient{
+
+    private axiosClient: AxiosInstance
+
+    constructor(){
+        this.axiosClient = axios.create({
+            baseURL:'http://localhost:8080/api/condutor',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+    public async findById(id:number): Promise<Condutor>{
+        try{
+            return(await this.axiosClient.get<Condutor>(`/${id}`)).data
+        } 
+        catch(error: any){
+            return Promise.reject(error.response)
+        }
+    }
+    public async cadastrar(condutor:Condutor): Promise<void>{
+        try{
+            return (await this.axiosClient.post('/', condutor))
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+    public async editar(condutor:Condutor): Promise<void>{
+        try{
+            return (await this.axiosClient.put(`/${condutor.id}`, condutor)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+}
